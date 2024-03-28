2e7f1eea0cb1853f4a86291a9bc19d18d6264bbf
med-aziz-guennichi <medazizguennichi@gmail.com>
2024-03-28 23:09:20 +0100

    add the user service and the db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/med-aziz-guennichi/golang-ecommerce/cmd/api"
+	"github.com/med-aziz-guennichi/golang-ecommerce/config"
+	"github.com/med-aziz-guennichi/golang-ecommerce/db"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAdress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	initStorage(db)
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB: Successfully connected!")
 }
